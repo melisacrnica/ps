e2fc0442c9e389cdcae4f6cfab1f00d0508153c8
BrandonMagana <magana.brandon12@outlook.com>
2022-06-03 14:05:52 -0500

    Portfolio landing page  updated

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
+description = "I'm a second year college student majoring in Software Engineering, and I am aproduction Engineer Fellow at MLH this summer!"
+
+
 @app.route('/')
 def landing_page():
-    return render_template('landingPage.html', name= 'Brandon Magaña', major = 'Software Engineering',profile_photo='static/img/logo.jpg', url=os.getenv('URL'))
+    return render_template('landingPage.html', name= 'Brandon Magaña', description = description,profile_photo='static/img/logo.jpg', landing_page=True, url=os.getenv('URL'))
 
 @app.route('/about-me')
 def about_me():
-    return render_template('about-me.html', url=os.getenv('URL'))
+    return render_template('about-me.html', landing_page=False, url=os.getenv('URL'))
 
 @app.route('/contact-me')
 def contact_me():
