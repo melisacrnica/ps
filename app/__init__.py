59f1c4e77a308a6f63722c6e4e9692f3e3bc808a
BrandonMagana <magana.brandon12@outlook.com>
2022-06-03 00:01:16 -0500

    First prototype for porfolio website

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,22 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
+def landing_page():
+    return render_template('landingPage.html', name= 'Brandon Magaña', major = 'Software Engineering',profile_photo='static/img/logo.jpg', url=os.getenv('URL'))
+
+@app.route('/about-me')
+def about_me():
+    return render_template('about-me.html', url=os.getenv('URL'))
+
+@app.route('/contact-me')
+def contact_me():
+    return render_template('contact-me.html', url=os.getenv('URL'))
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html', url=os.getenv('URL'))
+
+@app.route('/education')
+def education():
+    return render_template('education.html', url=os.getenv('URL'))
